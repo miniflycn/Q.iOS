633e57e5c51400e9466e5a256ce87d7f04daf6e7
Daniel Yang <miniflycn@justany.net>
2015-10-01 21:15:57 +0800

    Initial Commit

---
+++ Q.iOSTests/Q_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Q_iOSTests.swift
+//  Q.iOSTests
+//
+//  Created by 杨 文坚 on 15/10/1.
+//  Copyright (c) 2015年 miniflycn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Q_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
